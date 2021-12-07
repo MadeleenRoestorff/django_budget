848bcd7971868aac87ee6cf7e3808ee7367cf197
madeleen <maria.roestorff@gmail.com>
2021-12-07 15:35:09 +0200

    BUD-2 update git ignore: not files in migration

--- django_budget/scripts/nakedDB.sh
+++ django_budget/scripts/nakedDB.sh
@@ -6,5 +6,16 @@ rm db.sqlite3
 
 echo "Cleaning Migrations"
 
-rm ./budget/Migrations/*
+rm ./budget/migrations/*
 
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py createsuperuser --email admin@example.com --username admin --password 2JDYuSf4ttHZgFB
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+python3 manage.py runserver
