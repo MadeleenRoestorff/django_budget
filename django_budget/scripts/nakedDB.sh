4f30de096b5f1ce8169c7b81633c814cda97bfb7
madeleen <maria.roestorff@gmail.com>
2021-12-07 15:55:56 +0200

    BUD-2 added automation script

--- django_budget/scripts/nakedDB.sh
+++ django_budget/scripts/nakedDB.sh
@@ -12,7 +12,8 @@ python3 manage.py makemigrations
 
 python3 manage.py migrate
 
-python3 manage.py createsuperuser --email admin@example.com --username admin --password 2JDYuSf4ttHZgFB
+DJANGO_SUPERUSER_PASSWORD=2JDYuSf4ttHZgFB \
+python3 manage.py createsuperuser --email admin@example.com --username admin --no-input
 
 python3 manage.py makemigrations
 
