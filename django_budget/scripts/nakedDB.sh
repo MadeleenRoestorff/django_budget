19fe2a737f552b12aec08c7339b175be3834c1b9
madeleen <maria.roestorff@gmail.com>
2022-04-08 16:45:44 +0200

    added dockerfile and docker compose and requirements

--- django_budget/scripts/nakedDB.sh
+++ django_budget/scripts/nakedDB.sh
@@ -5,6 +5,7 @@ echo "No turning back, deleting the database"
 # python3 -m webbrowser -n https://stackoverflow.com &
 
 sensible-browser http://127.0.0.1:8000/admin/budget &
+# sensible-browser http://localhost:8000/admin/budget/ &
 
 # ps -aux
 # sudo kill 12345
